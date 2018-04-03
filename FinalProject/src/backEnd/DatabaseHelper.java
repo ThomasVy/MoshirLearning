2677f5b44a567641ee8e306ec30d39a39b9cedc6
limra <limra@10.13.158.205>
2018-04-03 14:42:09 -0600

    Tables have been made

--- FinalProject/src/backEnd/DatabaseHelper.java
+++ FinalProject/src/backEnd/DatabaseHelper.java
@@ -2,81 +2,148 @@ package backEnd;
 
 import java.sql.*;
 
-import sharedElements.LoginInfo;
-
 public class DatabaseHelper implements ConnectionConstant {
+
 	private Connection connection;
 	private Statement statement;
 	private ResultSet resultSet;
-	private String tableName;
-	private LoginInfo loginInformation;
-	
-//	protected abstract void insert(Object e);
-//
-//	protected abstract void delete(Object e);
-//
-//	protected abstract void select(Object e);
-//	
-	protected DatabaseHelper (LoginInfo login)
-	{
+
+	public DatabaseHelper() {
 		try {
-			loginInformation = login;
 			Class.forName("com.mysql.jdbc.Driver");
-			
-			connection = DriverManager.getConnection(connectionInfo, login.getUsername(), login.getPassword());
+			connection = DriverManager.getConnection(connectionInfo, "root", "30017106mysql");
 			System.out.println("Connected to: " + connectionInfo + "\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch(SQLException e) { e.printStackTrace(); }
-		catch(Exception e) { e.printStackTrace(); }
 	}
-	public void createDB ()
-	{
+
+	public void createDB() {
 		try {
 			statement = connection.createStatement();
-			String query = "CREATE DATABASE " + databaseName;
-			statement.executeUpdate(query);
-			System.out.println("Created Database " + databaseName);
+			statement.executeUpdate("CREATE DATABASE " + databaseName);
+			System.out.println("Created database " + databaseName);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch(SQLException e) { e.printStackTrace(); }
-		catch(Exception e) { e.printStackTrace(); }
 	}
-	public void createUserTable()
-	{
-		String sql = "CREATE TABLE " + tableName + "(" +
-			     "ID INT(4) NOT NULL, " +
-			     "TOOLNAME VARCHAR(20) NOT NULL, " + 
-			     "QUANTITY INT(4) NOT NULL, " + 
-			     "PRICE DOUBLE(5,2) NOT NULL, " + 
-			     "SUPPLIERID INT(4) NOT NULL, " + 
-			     "PRIMARY KEY ( id ))";
-		try{
-		statement = jdbc_connection.prepareStatement(sql);
-		statement.executeUpdate();
-		System.out.println("Created Table " + tableName);
+
+	public void createUserTable() {
+		String sql = "CREATE TABLE " + "UserTable" + " (id INT(8) NOT NULL," +
+													 " username VARCHAR(20) NOT NULL," +
+													 " password VARCHAR(20) NOT NULL," +
+													 " email VARCHAR(50) NOT NULL," +
+													 " firstname VARCHAR(30) NOT NULL," +
+													 " lastname VARCHAR(30) NOT NULL," +
+													 " type CHAR(1) NOT NULL," +
+													 " PRIMARY KEY (id))";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Created table " + "UserTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	catch(SQLException e)
-	{
-		e.printStackTrace();
+
+	public void createCourseTable() {
+		String sql = "CREATE TABLE " + "CourseTable" + " (id INT(8) NOT NULL," +
+													   " prof_id INT(8) NOT NULL," +
+													   " name VARCHAR(50) NOT NULL," +
+													   " active BIT(1) NOT NULL," +
+													   " PRIMARY KEY (id))";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Create table " + "CourseTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public void createEnrollmentTable() {
+		String sql = "CREATE TABLE " + "EnrollmentTable" + " (id INT(8) NOT NULL," +
+				   										   " student_id INT(8) NOT NULL," +
+				   										   " course_id INT(8) NOT NULL," +
+				   										   " PRIMARY KEY (id))";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Create table " + "EnrollmentTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	public void createCourseTable()
-	{
-		
+
+	public void createAssignmentTable() {
+		String sql = "CREATE TABLE " + "AssignmentTable" + " (id INT(8) NOT NULL," +
+				   										   " course_id INT(8) NOT NULL," +
+				   										   " title VARCHAR(50) NOT NULL," +
+				   										   " path VARCHAR(100) NOT NULL," +
+				   										   " active BIT(1) NOT NULL," +
+				   										   " due_date CHAR(16) NOT NULL," +
+				   										   " PRIMARY KEY (id))";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Create table " + "AssignmentTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	public void createEnrollmentTable()
-	{
-		
+
+	public void createSubmissionTable() {
+		String sql = "CREATE TABLE " + "SubmissionTable" + " (id INT(8) NOT NULL," +
+				   										   " assign_id INT(8) NOT NULL," +
+				   										   " student_id INT(8) NOT NULL," +
+				   										   " path VARCHAR(100) NOT NULL," +
+				   										   " title VARCHAR(50) NOT NULL," +
+				   										   " submission_grade INT(3) NOT NULL," +
+				   										   " comments VARCHAR(140) NOT NULL," +
+				   										   " timestamp CHAR(16) NOT NULL," +
+				   										   " PRIMARY KEY (id))";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Create table " + "SubmissionTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	public void createAssignmentTable()
-	{
-		
+
+	public void createGradeTable() {
+		String sql = "CREATE TABLE " + "GradeTable" + " (id INT(8) NOT NULL," +
+				   									  " assign_id INT(8) NOT NULL," +
+				   									  " student_id INT(8) NOT NULL," +
+				   									  " course_id INT(8) NOT NULL," +
+				   									  " assignment_grade INT(3) NOT NULL," +
+				   									  " PRIMARY KEY (id))";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Create table " + "GradeTable");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	public void createSubmissionTable()
-	{
-		
+
+	public void createAllTables() {
+		createUserTable();
+		createCourseTable();
+		createEnrollmentTable();
+		createAssignmentTable();
+		createSubmissionTable();
+		createGradeTable();
 	}
-	public void createGradeTable()
-	{
-		
+
+	public static void main(String[] args) {
+		DatabaseHelper dbh = new DatabaseHelper();
+		// dbh.createDB();
+		// dbh.createAllTables();
 	}
+
 }
